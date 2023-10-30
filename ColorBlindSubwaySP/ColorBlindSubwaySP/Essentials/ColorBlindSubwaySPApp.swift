339d9105bad9dca1e51b620d32b99d9c960540b6
Henrique Assis <henriquebatista061@gmail.com>
2023-10-30 15:02:55 -0300

    feat: projeto criado

---
+++ ColorBlindSubwaySP/ColorBlindSubwaySP/Essentials/ColorBlindSubwaySPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorBlindSubwaySPApp.swift
+//  ColorBlindSubwaySP
+//
+//  Created by Henrique Assis on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorBlindSubwaySPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorBlindSubwaySP/ColorBlindSubwaySP/Essentials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorBlindSubwaySP
+//
+//  Created by Henrique Assis on 30/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
