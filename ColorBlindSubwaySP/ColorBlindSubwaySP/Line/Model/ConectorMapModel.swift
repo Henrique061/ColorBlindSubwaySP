ebcf7261273a67f1fe59f3868a2eb7715bac25af
Henrique Assis <henriquebatista061@gmail.com>
2023-11-13 17:47:51 -0300

    Update ConectorMapModel.swift

--- ColorBlindSubwaySP/ColorBlindSubwaySP/Line/Model/ConectorMapModel.swift
+++ ColorBlindSubwaySP/ColorBlindSubwaySP/Line/Model/ConectorMapModel.swift
@@ -43,21 +43,21 @@ extension ConnectorMapModel {
         ConnectorMapModel(connectedLines: Set([.safira_12, .coral_11, .vermelha_3]), imageName: "ConectorTriplo", positionMultiplier: .init(x: 0.755,  y: 0.392)), // tatuape
         
         // DUPLOS
-        ConnectorMapModel(connectedLines: Set([.azul_1, .lilas_5]),         positionMultiplier: .init(x: 0.511,  y: 0.860), rotation: 90),  // santa cruz
-        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .lilas_5]),    positionMultiplier: .init(x: 0.073,  y: 0.974), rotation: 135), // santo amaro
-        ConnectorMapModel(connectedLines: Set([.verde_2, .lilas_5]),        positionMultiplier: .init(x: 0.582,  y: 0.804), rotation: 45),  // chacara kablin
-        ConnectorMapModel(connectedLines: Set([.verde_2, .azul_1]),         positionMultiplier: .init(x: 0.494,  y: 0.774), rotation: 90),  // ana rosa
-        ConnectorMapModel(connectedLines: Set([.verde_2, .azul_1]),         positionMultiplier: .init(x: 0.494,  y: 0.735), rotation: 90),  // paraiso
-        ConnectorMapModel(connectedLines: Set([.verde_2, .prata_15]),       positionMultiplier: .init(x: 0.883,  y: 0.606), rotation: 0),   // vila prudente
-        ConnectorMapModel(connectedLines: Set([.verde_2, .turquesa_10]),    positionMultiplier: .init(x: 0.863,  y: 0.646), rotation: 90),  // tamanduatei
-        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .amarela_4]),  positionMultiplier: .init(x: 0.080,  y: 0.590), rotation: 0),   // pinheiros
-        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .diamante_8]), positionMultiplier: .init(x: 0.057,  y: 0.385), rotation: 0),   // pres. altino
-        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .diamante_8]), positionMultiplier: .init(x: 0.023,  y: 0.385), rotation: 0),   // osasco
-        ConnectorMapModel(connectedLines: Set([.verde_2, .amarela_4]),      positionMultiplier: .init(x: 0.309,  y: 0.568), rotation: 90),  // paulista/consolacao
-        ConnectorMapModel(connectedLines: Set([.vermelha_3, .amarela_4]),   positionMultiplier: .init(x: 0.412,  y: 0.447), rotation: 0),   // republica
-        ConnectorMapModel(connectedLines: Set([.vermelha_3, .azul_1]),      positionMultiplier: .init(x: 0.5025, y: 0.459), rotation: 0),   // se
-        ConnectorMapModel(connectedLines: Set([.safira_12, .jade_13]),      positionMultiplier: .init(x: 0.818,  y: 0.310), rotation: 135), // eng goulart
-        ConnectorMapModel(connectedLines: Set([.safira_12, .coral_11]),     positionMultiplier: .init(x: 1.222,  y: 0.319), rotation: 0),   // calmon viana
-        ConnectorMapModel(connectedLines: Set([.coral_11, .vermelha_3]),    positionMultiplier: .init(x: 1.010,  y: 0.4005), rotation: 0)   // corinthians - itaquera
+        ConnectorMapModel(connectedLines: Set([.azul_1, .lilas_5]),         positionMultiplier: .init(x: 0.511,  y: 0.860),  rotation: 90),  // santa cruz
+        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .lilas_5]),    positionMultiplier: .init(x: 0.073,  y: 0.974),  rotation: 135), // santo amaro
+        ConnectorMapModel(connectedLines: Set([.verde_2, .lilas_5]),        positionMultiplier: .init(x: 0.582,  y: 0.804),  rotation: 45),  // chacara kablin
+        ConnectorMapModel(connectedLines: Set([.verde_2, .azul_1]),         positionMultiplier: .init(x: 0.494,  y: 0.774),  rotation: 90),  // ana rosa
+        ConnectorMapModel(connectedLines: Set([.verde_2, .azul_1]),         positionMultiplier: .init(x: 0.494,  y: 0.735),  rotation: 90),  // paraiso
+        ConnectorMapModel(connectedLines: Set([.verde_2, .prata_15]),       positionMultiplier: .init(x: 0.883,  y: 0.606),  rotation: 0),   // vila prudente
+        ConnectorMapModel(connectedLines: Set([.verde_2, .turquesa_10]),    positionMultiplier: .init(x: 0.863,  y: 0.646),  rotation: 90),  // tamanduatei
+        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .amarela_4]),  positionMultiplier: .init(x: 0.080,  y: 0.590),  rotation: 0),   // pinheiros
+        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .diamante_8]), positionMultiplier: .init(x: 0.057,  y: 0.385),  rotation: 0),   // pres. altino
+        ConnectorMapModel(connectedLines: Set([.esmeralda_9, .diamante_8]), positionMultiplier: .init(x: 0.023,  y: 0.385),  rotation: 0),   // osasco
+        ConnectorMapModel(connectedLines: Set([.verde_2, .amarela_4]),      positionMultiplier: .init(x: 0.309,  y: 0.568),  rotation: 90),  // paulista/consolacao
+        ConnectorMapModel(connectedLines: Set([.vermelha_3, .amarela_4]),   positionMultiplier: .init(x: 0.412,  y: 0.447),  rotation: 0),   // republica
+        ConnectorMapModel(connectedLines: Set([.vermelha_3, .azul_1]),      positionMultiplier: .init(x: 0.5025, y: 0.459),  rotation: 0),   // se
+        ConnectorMapModel(connectedLines: Set([.safira_12, .jade_13]),      positionMultiplier: .init(x: 0.818,  y: 0.310),  rotation: 135), // eng goulart
+        ConnectorMapModel(connectedLines: Set([.safira_12, .coral_11]),     positionMultiplier: .init(x: 1.222,  y: 0.319),  rotation: 0),   // calmon viana
+        ConnectorMapModel(connectedLines: Set([.coral_11, .vermelha_3]),    positionMultiplier: .init(x: 1.010,  y: 0.4005), rotation: 0)    // corinthians - itaquera
     ]
 }
